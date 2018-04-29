f2533ce28250b541c37429f6588951949bbd3af4
EliotChignell <chignelleliot@gmail.com>
2018-04-29 17:19:55 +1200

    update

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 var p = {
-    x: 42.5,
-    y: 400,
+    x: 230,
+    y: 430,
     w: 40,
     h: 40,
     colour: 'white'
 };
+var j = {
+    score: 0,
+    lasers: []
+};
+
+// Lasers
+function laser() {
+    this.x = p.x + 10;
+    this.y = 430;
+    this.ys = 10; // ys = y speed
+    this.colour = 'deepskyblue';
+    this.draw = function() {
+        fill(this.colour);
+        rect(this.x, this.y, 20, 40);
+    }
+    this.update = function() {
+        this.y -= this.ys;
+    }
+}
+
+function dLasers() {
+    for (i in j.lasers) {
+        j.lasers[i].draw();
+        j.lasers[i].update();
+    }
+}
 
 function setup() {
     createCanvas(500,500);
@@ -14,16 +40,37 @@ function setup() {
 function draw() {
     clear();
     background(0);
+    // Lasers
+    dLasers();
+
+    // Presets
+    fill(90);
+    rect(30,430,40,40);
+    rect(130,430,40,40);
+    rect(230,430,40,40);
+    rect(330,430,40,40);
+    rect(430,430,40,40);
+    rect(0,390,500,10);
+    // Scoreboard
+
     // Player
     fill(p.colour);
     rect(p.x,p.y,p.w,p.h);
 }
 
 function keyPressed() {
-    if (keyCode === LEFT_ARROW && p.x > 0) {
-        p.x -= 125;
+    if ((keyCode === LEFT_ARROW || keyCode === 65) && p.x > 30) {
+        p.x -= 100;
     }
-    if (keyCode === RIGHT_ARROW && p.x < 500-p.w) {
-        p.x += 125;
+    if ((keyCode === RIGHT_ARROW || keyCode === 68) && p.x < 430) {
+        p.x += 100;
     }
+    if ((keyCode === UP_ARROW || keyCode === 87 || keyCode === 32)) {
+        shootLaser();
+    }
+    console.log(p.x);
+}
+
+function shootLaser() {
+    j.lasers.push(new laser());
 }
